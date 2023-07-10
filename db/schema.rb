bf4482ed14cd65a801965000535cda9d2061e4d9
Kagheni <Kaghenimbale@gmail.com>
2023-07-10 15:19:33 +0200

    Add food model

--- db/schema.rb
+++ db/schema.rb
@@ -15,24 +15,22 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_10_091342) do
   enable_extension "plpgsql"
 
   create_table "foods", force: :cascade do |t|
+    t.bigint "user_id"
     t.string "name"
     t.string "measurement_unit"
     t.integer "price"
     t.integer "quantity"
-    t.bigint "user_id_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id_id"], name: "index_foods_on_user_id_id"
+    t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
   create_table "recipe_foods", force: :cascade do |t|
     t.string "quantity"
-    t.bigint "recipe_id_id", null: false
+    t.bigint "recipe_id", null: false
     t.bigint "food_id_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["food_id_id"], name: "index_recipe_foods_on_food_id_id"
-    t.index ["recipe_id_id"], name: "index_recipe_foods_on_recipe_id_id"
+    t.index ["food_id"], name: "index_recipe_foods_on_food_id"
+    t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -41,10 +39,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_10_091342) do
     t.decimal "cooking_time"
     t.text "description"
     t.boolean "public"
-    t.bigint "user_id_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id_id"], name: "index_recipes_on_user_id_id"
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -65,8 +63,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_10_091342) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "foods", "users", column: "user_id_id"
-  add_foreign_key "recipe_foods", "foods", column: "food_id_id"
-  add_foreign_key "recipe_foods", "recipes", column: "recipe_id_id"
-  add_foreign_key "recipes", "users", column: "user_id_id"
+  add_foreign_key "foods", "users", column: "user_id"
+  add_foreign_key "recipe_foods", "foods", column: "food_id"
+  add_foreign_key "recipe_foods", "recipes", column: "recipe_id"
+  add_foreign_key "recipes", "users", column: "user_id"
 end
