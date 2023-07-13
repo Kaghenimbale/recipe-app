f2ea9dbfd960dfa41d1ada2a1c3c6a0508d97413
evillaz <evillaz123@gmail.com>
2023-07-13 05:12:03 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,16 @@ Rails.application.routes.draw do
 
   resources :users
   resources :foods, only: [:index, :new, :create, :destroy]
-
   # Defines the root path route ("/")
   # root "foods#index"
   root "home#index"
-  resources :recipes
+  resources :recipes do
+    member do
+      post 'toggle'
+    end
+    resources :recipe_foods, only: [:new, :create, :update, :destroy] do
+      get :modify, on: :member
+    end
+  end
+  resources :public_recipes, only: [:index]
 end
