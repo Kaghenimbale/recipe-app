8682dfd6a7bc8d06c1a6ce74c62b57bfe96e1f64
evillaz <evillaz123@gmail.com>
2023-07-13 08:10:25 -0500

    Shopping list update

--- app/controllers/general_shopping_list_controller.rb
+++ app/controllers/general_shopping_list_controller.rb
@@ -1,10 +1,16 @@
 class GeneralShoppingListController < ApplicationController
   before_action :authenticate_user!
   def index
-    @foods = Food.all
-    @total = 0
-    current_user.foods.each do |food|
-      @total += food.price * food.quantity
-    end
+    @user = current_user
+    @foods = @user.foods
+    @recipe_foods = @user.recipes.joins(recipe_foods: :food)
+                    .select("foods.name AS food_name, foods.price AS food_price, foods.quantity AS food_quantity, SUM(recipe_foods.quantity) AS total_amount")
+                    .group("foods.name, foods.price, foods.quantity")
+                    .to_a
+
+    ##@total = 0
+    #current_user.foods.each do |food|
+    #  @total += food.price * food.quantity
+    #end
   end
 end
