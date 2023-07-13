7fe53c83818f68325edb419a9f8ff4a4d53f3042
evillaz <evillaz123@gmail.com>
2023-07-13 05:08:12 -0500

    Modify attribute value recipe foods

--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_10_091342) do
   end
 
   create_table "recipe_foods", force: :cascade do |t|
-    t.string "quantity"
+    t.integer "quantity"
     t.bigint "recipe_id", null: false
     t.bigint "food_id", null: false
     t.datetime "created_at", null: false
